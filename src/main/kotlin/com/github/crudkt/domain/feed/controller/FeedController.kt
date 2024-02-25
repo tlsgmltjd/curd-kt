fcdf447f83876af1aa7c87433a3f4df8a81e5e9d
신희성 <vvwv3092@gmail.com>
2024-02-26 00:02:11 +0900

    Add save feed controller

---
+++ src/main/kotlin/com/github/crudkt/domain/feed/controller/FeedController.kt
@@ -0,0 +1,23 @@
+package com.github.crudkt.domain.feed.controller
+
+import com.github.crudkt.domain.feed.controller.dto.SaveFeedRequest
+import com.github.crudkt.domain.feed.service.SaveFeedService
+import org.springframework.http.HttpStatus.*
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/feeds")
+class FeedController(
+    private val saveFeedService: SaveFeedService
+) {
+
+    @PostMapping
+    fun saveFeed(@RequestBody dto: SaveFeedRequest): ResponseEntity<Void> {
+        saveFeedService.execute(dto);
+        return ResponseEntity.status(CREATED).build();
+    }
+}
