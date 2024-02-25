ee9970fa72d6018be0fc0050fd481da2b20a881e
신희성 <vvwv3092@gmail.com>
2024-02-25 23:32:52 +0900

    init

---
+++ src/test/kotlin/com/github/crudkt/CrudKtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.crudkt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudKtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
