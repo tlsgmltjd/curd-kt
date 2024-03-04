d74890b5d009d0a0cb61f4ba18d1dd0abdd52062
신희성 <vvwv3092@gmail.com>
2024-03-04 23:14:49 +0900

    Add ApplicationEventPublisher

--- src/main/kotlin/com/github/crudkt/domain/feed/service/SaveFeedService.kt
+++ src/main/kotlin/com/github/crudkt/domain/feed/service/SaveFeedService.kt
@@ -3,13 +3,16 @@ package com.github.crudkt.domain.feed.service
 import com.github.crudkt.domain.feed.controller.dto.SaveFeedRequest
 import com.github.crudkt.domain.feed.domain.FeedEntity
 import com.github.crudkt.domain.feed.repository.FeedJpaRepository
+import com.github.crudkt.domain.ticket.event.TicketEvent
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional
 class SaveFeedService(
-    private val feedJpaRepository: FeedJpaRepository
+    private val feedJpaRepository: FeedJpaRepository,
+    private val publisher: ApplicationEventPublisher
 ) {
     fun execute(dto: SaveFeedRequest) {
          val feedEntity = FeedEntity(
@@ -17,6 +20,12 @@ class SaveFeedService(
              content = dto.content
          )
 
-        feedJpaRepository.save(feedEntity)
+        val savedFeedEntity = feedJpaRepository.save(feedEntity)
+
+        publish(savedFeedEntity);
+    }
+
+    private fun publish(savedFeedEntity: FeedEntity) {
+        publisher.publishEvent(TicketEvent(savedFeedEntity))
     }
 }
