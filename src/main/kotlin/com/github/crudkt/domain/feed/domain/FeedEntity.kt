fb98ad53f6966c7fcc0239752eb7358bf58df4a0
신희성 <vvwv3092@gmail.com>
2024-03-04 23:00:07 +0900

    Add Ticket Entity

--- src/main/kotlin/com/github/crudkt/domain/feed/domain/FeedEntity.kt
+++ src/main/kotlin/com/github/crudkt/domain/feed/domain/FeedEntity.kt
@@ -1,17 +1,21 @@
 package com.github.crudkt.domain.feed.domain
 
 import com.github.crudkt.domain.feed.controller.dto.UpdateFeedRequest
+import com.github.crudkt.domain.ticket.domain.TicketEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToOne
 
 @Entity
 class FeedEntity (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val feedId: Long = 0,
     var title: String,
-    var content: String
+    var content: String,
+    @OneToOne(mappedBy = "feed")
+    val ticket: TicketEntity
 ) {
     fun updateFeed(dto: UpdateFeedRequest) {
         this.title = dto.title
---
+++ src/main/kotlin/com/github/crudkt/domain/ticket/domain/TicketEntity.kt
@@ -0,0 +1,17 @@
+package com.github.crudkt.domain.ticket.domain
+
+import com.github.crudkt.domain.feed.domain.FeedEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToOne
+
+@Entity
+class TicketEntity (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val ticketId: Long,
+    val ticketContent: String,
+    @OneToOne
+    val feed: FeedEntity
+)
