017c9fb97ff7a6bd7edfcc8fddc64f2a681f3b04
신희성 <vvwv3092@gmail.com>
2024-04-26 12:07:32 +0900

    Add CommentController

--- src/main/kotlin/com/github/crudkt/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/github/crudkt/domain/comment/controller/CommentController.kt
@@ -1,4 +1,21 @@
 package com.github.crudkt.domain.comment.controller
 
-class CommentController {
+import com.github.crudkt.domain.comment.controller.dto.SaveCommentRequest
+import com.github.crudkt.domain.comment.service.SaveCommentService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/comments")
+class CommentController (
+    private val saveCommentService: SaveCommentService
+) {
+    @PostMapping
+    fun saveComment(@RequestBody saveCommentRequest: SaveCommentRequest): ResponseEntity<Void> {
+        saveCommentService.execute(saveCommentRequest)
+        return ResponseEntity.ok().build()
+    }
 }
