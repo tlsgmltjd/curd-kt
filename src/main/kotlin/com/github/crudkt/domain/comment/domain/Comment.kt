28d8bfd9e73c9bcecd493ebb996b8be819b675b8
신희성 <vvwv3092@gmail.com>
2024-05-01 11:06:52 +0900

    Update feed - comment cascade

--- src/main/kotlin/com/github/crudkt/domain/comment/domain/Comment.kt
+++ src/main/kotlin/com/github/crudkt/domain/comment/domain/Comment.kt
@@ -2,15 +2,14 @@ package com.github.crudkt.domain.comment.domain
 
 import com.github.crudkt.domain.feed.domain.FeedEntity
 import jakarta.persistence.*
-import org.hibernate.annotations.Cascade
-import org.hibernate.annotations.CascadeType
 
 @Entity
 class Comment (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val commentId: Long = 0,
     val comment: String,
-    @ManyToOne
-    @Cascade(value = [CascadeType.ALL])
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
     val feed: FeedEntity
 )
--- src/main/kotlin/com/github/crudkt/domain/feed/domain/FeedEntity.kt
+++ src/main/kotlin/com/github/crudkt/domain/feed/domain/FeedEntity.kt
@@ -1,19 +1,25 @@
 package com.github.crudkt.domain.feed.domain
 
+import com.github.crudkt.domain.comment.domain.Comment
 import com.github.crudkt.domain.feed.controller.dto.UpdateFeedRequest
 import com.github.crudkt.domain.ticket.domain.TicketEntity
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.OneToOne
+import lombok.Builder
 
 @Entity
 class FeedEntity (
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val feedId: Long = 0,
     var title: String,
-    var content: String
+    var content: String,
+    @OneToMany(mappedBy = "feed", cascade = [CascadeType.ALL])
+    val comments: MutableList<Comment> = ArrayList()
 ) {
     fun updateFeed(dto: UpdateFeedRequest) {
         this.title = dto.title
